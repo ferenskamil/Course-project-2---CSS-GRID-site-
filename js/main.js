6f8670259fcb294bd9f2d066d9de0d484ebb22e2
Kamil <mr.kaam@gmail.com>
2023-02-17 15:14:22 +0100

    Structure:
    created site structure, nav and header

---
+++ js/main.js
@@ -0,0 +1,27 @@
+// VARIABLES
+const BURGER_BTN = document.querySelector('.hamburger');
+// const BURGER_BARS_ARR = document.querySelectorAll('')
+
+const NAV = document.querySelector('.nav');
+const FOOTER_YEAR_SPAN = document.querySelector('.footer__year');
+
+// FUNCTIONS
+const showHideNav = () => {
+	NAV.classList.toggle('nav-mobile--active');
+	changeBurgerIcon();
+};
+
+const changeBurgerIcon = () => {
+	console.log('test');
+	BURGER_BTN.classList.toggle('is-active');
+};
+
+const updateFooterYear = () => {
+	let currentYear = new Date().getFullYear();
+
+	FOOTER_YEAR_SPAN.textContent = currentYear;
+};
+
+// EVENTS
+BURGER_BTN.addEventListener('click', showHideNav);
+updateFooterYear();
